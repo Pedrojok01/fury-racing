203879b08b2e202118c042d85beb0ea9fd27e23c
Pedrojok01 <pedrojok@pm.me>
2024-05-26 21:21:19 +0700

    📝 (constants.ts): update DESCRIPTION to reflect the correct project scope

--- frontend/src/data/constants.ts
+++ frontend/src/data/constants.ts
@@ -3,12 +3,10 @@ import type { Abi } from "viem";
 import { RACING_ABI } from "./abi";
 
 export const TITLE = "Fury Racing";
-export const DESCRIPTION = "Onchain racing simulator leveraging Chainlink VRF V2.5 and Chainlink Functions.";
+export const DESCRIPTION = "Onchain racing manager leveraging Chainlink VRF V2.5 and Chainlink Functions.";
 
 export const URL = "https://fury-racing.vercel.app/";
 export const GITHUB_REPO = "https://github.com/Pedrojok01/chainlink-hackathon";
-
-export const GLTF_PATH_CAR = "./assets/low-poly/scene.gltf";
 
 export const LINK_TOKEN = {
   tesnet: "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846",
